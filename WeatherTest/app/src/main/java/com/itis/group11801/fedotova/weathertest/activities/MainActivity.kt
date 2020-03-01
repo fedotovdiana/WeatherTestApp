2b3a360074136fc0ae20fa2aac4da8b900d4e8fe
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-03-01 14:54:28 +0300

    rewrite to activities

--- WeatherTest/app/src/main/java/com/itis/group11801/fedotova/weathertest/activities/MainActivity.kt
+++ WeatherTest/app/src/main/java/com/itis/group11801/fedotova/weathertest/activities/MainActivity.kt
@@ -1,37 +1,84 @@
 package com.itis.group11801.fedotova.weathertest.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build.ID
 import android.os.Bundle
-import kotlinx.coroutines.*
+import android.util.Log
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.itis.group11801.fedotova.weathertest.R
-import com.itis.group11801.fedotova.weathertest.fragments.SearchFragment
+import com.itis.group11801.fedotova.weathertest.net.ApiFactory
+import com.itis.group11801.fedotova.weathertest.net.WeatherService
+import com.itis.group11801.fedotova.weathertest.recycler.CityAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
-//    private lateinit var service: WeatherService
+    lateinit var adapter: CityAdapter
+    lateinit var service: WeatherService
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().apply {
-            replace(
-                R.id.container,
-                SearchFragment.newInstance()
-            )
-            addToBackStack(SearchFragment::class.java.name)
-            commit()
-        }
-//        service = ApiFactory.weatherService
-//
-//        launch {
-//            val response = withContext(Dispatchers.IO) {
-//                service.weatherByName("Kazan")
-//            }
-//            Toast.makeText(this@MainActivity, response.main.toString(), Toast.LENGTH_SHORT)
-//
-//        }
+        service = ApiFactory.weatherService
+        initSearchView()
 
+        val data: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        adapter = CityAdapter(data) {
+            showDetails(it)
+        }
+        rv_cities.adapter = adapter
+    }
+
+    private fun showDetails(id: Int) {
+        val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+        intent.putExtra(ID, id)
+        startActivity(intent)
+    }
+
+    private fun initSearchView() {
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    find(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun find(query: String): Boolean {
+        try {
+            launch {
+                val response = withContext(Dispatchers.IO) {
+                    service.weatherByName(query)
+                }
+                showDetails(response.id)
+            }
+        } catch (e: HttpException) {
+            Snackbar.make(main_layout, getString(R.string.tv_error), Snackbar.LENGTH_LONG).show()
+            Log.e("EXCEPTION", "$e")
+        }
+        return false
+    }
+
+    private fun findCities(lat: Double, lon: Double): Boolean {
+        launch {
+            val response = withContext(Dispatchers.IO) {
+                service.findCitiesInCycle(lat, lon)
+            }
+            //TODO addToList
+        }
+        return false
     }
 }
